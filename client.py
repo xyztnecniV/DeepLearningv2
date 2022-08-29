75c6d4193448d383f0f57bab1be5b1db23d5ee4b
Vincentzyx <929403983@qq.com>
2022-08-29 11:24:22 +0800

    Initial commit

---
+++ client.py
@@ -0,0 +1,10 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description="分布式生成棋谱")
+parser.add_argument("--user", default="VinXiangQi", type=str, help="用于统计训练量的用户名")
+parser.add_argument("--threads", default=-1, type=int, help="用于跑谱的核心数")
+Args = parser.parse_args()
+
+os.system("chmod +x ./deeplearning")
+os.system(f"./deeplearning -u {Args.user} -t {Args.threads}")
